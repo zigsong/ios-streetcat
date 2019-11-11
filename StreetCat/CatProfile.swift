3355b6085b10ad6a0f9ec24c059840907029e624
wanderlust16 <wldms5764@gmail.com>
2019-11-11 15:51:04 +0900

    initial commit

---
+++ StreetCat/CatProfile.swift
@@ -0,0 +1,37 @@
+//
+//  CatProfile.swift
+//  StreetCat
+//
+//  Created by songji on 09/11/2019.
+//  Copyright © 2019 songji. All rights reserved.
+//
+
+import UIKit
+
+//class CatProfile: Codable { // codable protocol 채택!
+//    // Properties
+//    var name: String?
+//    var photo: UIImage?
+//    var age: Int?
+//
+//    // Initialization
+//    init?(name: String?, photo: UIImage?, age: Int?) {
+//        // Error: Only a failable initializer can return 'nil' -> init? 으로 수정
+//        // Initialize stored properties
+//        self.name = name
+//        self.photo = photo
+//        self.age = age
+//
+//        // Initialization should fail if there is no name or if the rating is negative.
+//        if name!.isEmpty || age! < 0  { // !: 값이 있을 수도, 없을 수도 있음
+//            return nil
+//        }
+//    }
+//
+//    required init(from decoder: Decoder) throws {
+//        <#code#>
+//    }
+//}
+
+
+
---
+++ StreetCat/ViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ViewController.swift
+//  StreetCat
+//
+//  Created by songji on 08/11/2019.
+//  Copyright © 2019 songji. All rights reserved.
+//
+
+//import MapKit
+//
+//class ViewController: UIViewController {
+//    @IBOutlet weak var mapView: MKMapView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//    let location = CLLocationCoordinate2D(latitude: 51.50007773, longitude: -0.1246402)
+//
+//    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+//
+//    let region = MKCoordinateRegion(center: location, span: span)
+//        mapView.setRegion(region, animated: true)
+//
+//    let annotation = MKPointAnnotation()
+//       annotation.coordinate = location
+//       annotation.title = "Big Ben"
+//       annotation.subtitle = "London"
+//       mapView.addAnnotation(annotation)
+//
+//    }
+//}
+
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self // locatonManager가 현재 mapView에서 delegate처리를 할 수 있도록
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 위치 정확도 최고
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        self.mapView.delegate = self;
+        self.mapView.showsUserLocation = true // 현재위치를 마커로 표시 // CL과 함께 사용
+    }
+        
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//    }
+
+    func myLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta: Double) {
+        let coordinateLocation = CLLocationCoordinate2DMake(latitude, longitude)
+        let spanValue = MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
+        // 기존 함수: MKCoordinateSpanMake(delta, delta)
+        let locationRegion = MKCoordinateRegion(center: coordinateLocation, span: spanValue)
+        // 기존함수: MKCoordinateRegionMake(coordinateLocation, spanValue)
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let lastLocation = locations.last
+        myLocation(latitude: (lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!, delta: 0.01)
+        // delta값이 1보다 작을수록 확대됨. 100배 확대
+    }
+    
+
+//        self.locationManager.requestWhenInUseAuthorization() // .requestAlwaysAuthorization()
+//        var currentLoc: CLLocation!
+//        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+//        CLLocationManager.authorizationStatus() == .authorizedAlways) {
+            // 인증 상태가 유효하다면(위치 항상 허용/ 앱을 사용하는 동안)
+//        if CLLocationManager.locationServicesEnabled() {
+//            currentLoc = locationManager.location
+//            print(currentLoc.coordinate.latitude)
+//            print(currentLoc.coordinate.longitude)
+//        } else {
+//            print("error")
+//        }
+   
+}
