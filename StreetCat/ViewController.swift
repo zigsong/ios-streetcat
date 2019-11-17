9e7e018a5afa6c7ca5e023ecd06eb0df295cbea2
hyemmie <lhm0710@naver.com>
2019-11-17 15:53:45 +0900

    first commit

--- StreetCat/ViewController.swift
+++ StreetCat/ViewController.swift
@@ -55,21 +55,38 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
 //        super.didReceiveMemoryWarning()
 //    }
 
-    func myLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta: Double) {
-        let coordinateLocation = CLLocationCoordinate2DMake(latitude, longitude)
-        let spanValue = MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
+    func myLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) -> CLLocationCoordinate2D {
+        let coordinateLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
+        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
         // 기존 함수: MKCoordinateSpanMake(delta, delta)
         let locationRegion = MKCoordinateRegion(center: coordinateLocation, span: spanValue)
         // 기존함수: MKCoordinateRegionMake(coordinateLocation, spanValue)
+        mapView.setRegion(locationRegion, animated: true)
+        return coordinateLocation
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let lastLocation = locations.last
-        myLocation(latitude: (lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!, delta: 0.01)
+        _ = myLocation(latitude: (lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!, delta: 0.01)
         // delta값이 1보다 작을수록 확대됨. 100배 확대
     }
     
-
+    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double, title strTitle: String, subtitble strSubtitle:String) {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = myLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
+        annotation.title = strTitle
+        annotation.subtitle = strSubtitle
+        myMap.addAnnotation(annotation)
+    }
+    
+    @IBAction func sgChange(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            locationManager.startUpdatingLocation()
+        } else if sender.selectedSegmentIndex == 1 {
+            setAnnotation(latitudeValue: 37.4812114, longitudeValue: 126.9527522, delta: 0.01, title: "설입냥", subtitble: "고양이설명텍스트")
+        }
+    }
+    
 //        self.locationManager.requestWhenInUseAuthorization() // .requestAlwaysAuthorization()
 //        var currentLoc: CLLocation!
 //        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
