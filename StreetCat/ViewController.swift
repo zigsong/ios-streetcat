364a5fb82f80a293e4b1ccd5b525813363292284
wanderlust16 <wldms5764@gmail.com>
2019-11-17 09:12:40 +0900

    json test

--- StreetCat/ViewController.swift
+++ StreetCat/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         locationManager.delegate = self // locatonManager가 현재 mapView에서 delegate처리할 수 있도록 설정
         locationManager.desiredAccuracy = kCLLocationAccuracyBest // 위치 정확도 최고
         locationManager.requestWhenInUseAuthorization()
@@ -26,61 +26,26 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         mapView.showsUserLocation = true // 현재위치를 마커로 표시(실제로 표시하지는 않음???) // CL과 함께 사용
         // print(self.mapView.isUserLocationVisible)
 
-//        func configureLocationServices() {
-//            locationManager.delegate = self
-//            let status = CLLocationManager.authorizationStatus()
-//            if status == .notDetermined {
-//                locationManager.requestWhenInUseAuthorization()
-//            } else if status == .authorizedAlways || status == .authorizedWhenInUse {
-//                beginLocationsUpdate(locationManager: locationManager)
-//            }
-//            print(status)
-//        }
-//
-//        func beginLocationsUpdate(locationManager: CLLocationManager) {
-//            // turn on setting to show location if authorized
-//            mapView.showsUserLocation = true
-//            // accuracy of location data can choose precision
-//            locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//            locationManager.startUpdatingLocation()
-//        }
-//
-//        configureLocationServices()
-    }
-    
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//    }
-
     func myLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta: Double) {
         let coordinateLocation = CLLocationCoordinate2DMake(latitude, longitude)
         let spanValue = MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
-        // 기존 함수: MKCoordinateSpanMake(delta, delta)
         let locationRegion = MKCoordinateRegion(center: coordinateLocation, span: spanValue)
         mapView.setRegion(locationRegion, animated: true)
-        // 기존함수: MKCoordinateRegionMake(coordinateLocation, spanValue)
-        
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let lastLocation = locations.last
         myLocation(latitude: (lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!, delta: 0.01)
-
         // delta값이 1보다 작을수록 확대됨. 100배 확대
     }
     
+    @IBAction makeMockData(_ sender: UIButton) {
 
-//        self.locationManager.requestWhenInUseAuthorization() // .requestAlwaysAuthorization()
-//        var currentLoc: CLLocation!
-//        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
-//        CLLocationManager.authorizationStatus() == .authorizedAlways) {
-            // 인증 상태가 유효하다면(위치 항상 허용/ 앱을 사용하는 동안)
-//        if CLLocationManager.locationServicesEnabled() {
-//            currentLoc = locationManager.location
-//            print(currentLoc.coordinate.latitude)
-//            print(currentLoc.coordinate.longitude)
-//        } else {
-//            print("error")
-//        }
-   
+        let jsonData = jsonString.data(using: .utf8)!
+        let cat1 = try! JSONDecoder().decode(Cat.self, from: jsonData)
+        print(cat1.name)
+
+    }
+
+    }
 }
--- StreetCat/catProfile.swift
+++ StreetCat/catProfile.swift
@@ -6,87 +6,41 @@
 //  Copyright © 2019 songji. All rights reserved.
 //
 
-import UIKit
+import Foundation // Model은 UI와 관련이 없으므로 UIKit이 아닌 Foundation을 import
 
-class Cat: Codable { // codable protocol 채택!
+struct Cat: Codable { // codable protocol 채택!
+    
     typealias CLLocationDegrees = Double
 
-    // Properties
-    var name: String?
-    var photo: UIImage?
-    var age: Int?
-    // var spot: CLLocationDegrees?
-
-    // Initialization
-    init?(name: String?, photo: UIImage?, age: Int?) {
-        // Error: Only a failable initializer can return 'nil' -> init? 으로 수정
-        // Initialize stored properties
-        self.name = name
-        self.photo = photo
-        self.age = age
-
-        // Initialization should fail if there is no name or if the rating is negative.
-        if name!.isEmpty || age! < 0  { // !(optional): 값이 있을 수도, 없을 수도
-            return nil
-        }
-    }
-
-    func encode(to encoder: Encoder) throws { // encodable protocol에 필요
-        var container = encoder.singleValueContainer() // required. 단일값
-        try container.encode(self)
-    }
+    var name: String
+    var age: Int
+    // var photo: UIImage
+    // var spot: CLLocationDegrees
 
     enum CodingKeys: String, CodingKey {
+        // key 이름 customizing. json에서 사용하는 key값이 다른 경우, case에 json key값을 부여할 수 있다.
         case name
-        case photo
         case age
     }
+    
+    let encoder = JSONEncoder()
+    func encode(to encoder: Encoder) throws { // encodable protocol에 필요
+//        let encoder = JSONEncoder()
+//        encoder.outputFormatting = .prettyPrinted
+        // QQ. struct 안에서 encode func을 구현하는 방법은?
 
-    func decoder() {
-        
     }
     
-    required init(from decoder: Decoder) throws { // decodable protocol에 필요
-        let values = try decoder.container(keyedBy: CodingKeys.self) // CodingKey: enum type으로 각 case 명시
-        try? self.decoder(values)
+    let decoder = JSONDecoder()
+    init(from decoder: Decoder) throws { // decodable protocol에 필요. JSON을 직접 decoding하는 것
+        let profile = try decoder.container(keyedBy: CodingKeys.self) // CodingKeys의 type 명시
+        // decode.container: (required) 디코더에 저장된 데이터를 주어진 key type으로 변환하여 container로서 보여줌 (json을 말하는 듯)
+        name = try! profile.decode(String.self, forKey: .name) // QQ. 소스들은 try? 사용
+        age = try! profile.decode(Int.self, forKey: .age)
     }
+
 }
 
-//if let data=data, let cat1 = try? decoder.decode(Cat.self, from: data){
-//        print(cat1.name)
-//        print(cat1.photo)
-//        print(cat1.age)
-//}
-//
-//let encoder = JSONEncoder()
-//let jsonData = try! encoder.encode()
 
-//// 출처: Zedd (Encoding)
-//let cat1 = Cat(name: "zig", photo: nil, age: 3)
-//
-//let encoder = JSONEncoder()
-//let jsonData = try? encoder.encode(cat1) // encoding 중 에러를 발생시킬 수 있기 때문에 try 사용
-//if let jsonData = jsonData, let jsonString = String(data: jsonData, encoding: .utf8){
-//    print(jsonString) //{"name":"zig","age":3}
-//}
-//encoder.outputFormatting = .prettyPrinted
-//
-//// 출처: Zedd (Dedcoding)
-//let decoder = JSONDecoder()
-//var data = jsonString.data(using: .utf8)
-//if let data = data, let myCat = try? decoder.decode(Person.self, from: data) {
-//    print(myCat.name) // zig
-//    print(myCat.age) // 3
-//}
-//
 
-let jsonString = """
-{
-    "name": "Zig",
-    "photo": nil,
-    "age": 3
-}
-"""
-let jsonData = jsonString.data(using: .utf8)!
-let cat1 = try! JSONDecoder().decode(Cat.self, from: jsonData)
-print(cat1.name ?? "defaultName")
+   
