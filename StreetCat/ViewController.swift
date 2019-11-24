8b5b5c9e5c57ccf944071ff5e6720bba1951ef2e
hyemmie <lhm0710@naver.com>
2019-11-24 22:53:58 +0900

    laaaast

--- StreetCat/ViewController.swift
+++ StreetCat/ViewController.swift
@@ -1,106 +1,99 @@
 //
 //  ViewController.swift
-//  StreetCat
+//  mapview
 //
-//  Created by songji on 08/11/2019.
-//  Copyright © 2019 songji. All rights reserved.
+//  Created by ihyemin on 2019. 11. 10..
+//  Copyright © 2019년 ihyemin. All rights reserved.
 //
-
-//import MapKit
-//
-//class ViewController: UIViewController {
-//    @IBOutlet weak var mapView: MKMapView!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//    let location = CLLocationCoordinate2D(latitude: 51.50007773, longitude: -0.1246402)
-//
-//    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-//
-//    let region = MKCoordinateRegion(center: location, span: span)
-//        mapView.setRegion(region, animated: true)
-//
-//    let annotation = MKPointAnnotation()
-//       annotation.coordinate = location
-//       annotation.title = "Big Ben"
-//       annotation.subtitle = "London"
-//       mapView.addAnnotation(annotation)
-//
-//    }
-//}
-
 
 import UIKit
 import MapKit
-import CoreLocation
 
-class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-    
-    @IBOutlet weak var mapView: MKMapView!
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var myMap: MKMapView!
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationManager.delegate = self // locatonManager가 현재 mapView에서 delegate처리를 할 수 있도록
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest // 위치 정확도 최고
+        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy=kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-        self.mapView.delegate = self;
-        self.mapView.showsUserLocation = true // 현재위치를 마커로 표시 // CL과 함께 사용
-    }
+        myMap.showsUserLocation = true
+        myMap.delegate = self
+//        setAnnotation(latitudeValue: 37.4812114, longitudeValue: 126.9527522, delta: 0.01, title: "설입냥", subtitble: "고양이설명텍스트")
         
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//    }
-
-    func myLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) -> CLLocationCoordinate2D {
-        let pLocation = CLLocationCoordinate2DMake(latitudeValue, longitudeValue)
-        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
-        // 기존 함수: MKCoordinateSpanMake(delta, delta)
-        let locationRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
-        // 기존함수: MKCoordinateRegionMake(coordinateLocation, spanValue)
-        mapView.setRegion(locationRegion, animated: true)
+        let cat = Cat(title: "설입냥",
+          locationName: "서울대입구",
+          discipline: "삼색냥이",
+          coordinate: CLLocationCoordinate2D(latitude: 37.4812114, longitude: 126.9527522))
+        myMap.addAnnotation(cat)
+    }
+    
+    
+    func goLocation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double) -> CLLocationCoordinate2D {
+        let pLocation = CLLocationCoordinate2DMake(latitudeValue,longitudeValue)
+//        let spanValue = MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+//        let pRegion = MKCoordinateRegion(center: pLocation, span: spanValue)
+       // myMap.setRegion(pRegion, animated: true)
         return pLocation
     }
-
-
+    
+//    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double, title strTitle: String, subtitble strSubtitle:String) {
+//        let annotation = MKPointAnnotation()
+//        annotation.coordinate = goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
+//        annotation.title = strTitle
+//        annotation.subtitle = strSubtitle
+//        myMap.addAnnotation(annotation)
+//    }
+    
+    
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let pLocation = locations.last
-        _ = myLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue: (pLocation?.coordinate.longitude)!, delta: 0.01)
-        // delta값이 1보다 작을수록 확대됨. 100배 확대
+        _ = goLocation(latitudeValue: (pLocation?.coordinate.latitude)!, longitudeValue:  (pLocation?.coordinate.longitude)!, delta: 0.01)
     }
     
-    func setAnnotation(latitudeValue: CLLocationDegrees, longitudeValue: CLLocationDegrees, delta span: Double, title strTitle: String, subtitble strSubtitle:String) {
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = myLocation(latitudeValue: latitudeValue, longitudeValue: longitudeValue, delta: span)
-        annotation.title = strTitle
-        annotation.subtitle = strSubtitle
-        mapView.addAnnotation(annotation)
-    }
-    
-    
-    
-//    @IBAction func sgChange(_ sender: UISegmentedControl) {
-//        if sender.selectedSegmentIndex == 0 {
-//            locationManager.startUpdatingLocation()
-//        } else if sender.selectedSegmentIndex == 1 {
-//            setAnnotation(latitudeValue: 37.4812114, longitudeValue: 126.9527522, delta: 0.01, title: "설입냥", subtitble: "고양이설명텍스트")
-//        }
-    }
-    
-//        self.locationManager.requestWhenInUseAuthorization() // .requestAlwaysAuthorization()
-//        var currentLoc: CLLocation!
-//        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
-//        CLLocationManager.authorizationStatus() == .authorizedAlways) {
-            // 인증 상태가 유효하다면(위치 항상 허용/ 앱을 사용하는 동안)
-//        if CLLocationManager.locationServicesEnabled() {
-//            currentLoc = locationManager.location
-//            print(currentLoc.coordinate.latitude)
-//            print(currentLoc.coordinate.longitude)
-//        } else {
-//            print("error")
-//        }
-   
 }
+        
+
+extension ViewController: MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+    guard let annotation = annotation as? Cat else { return nil }
+    let identifier = "marker"
+    var view: MKMarkerAnnotationView
+    if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+      as? MKMarkerAnnotationView {
+      dequeuedView.annotation = annotation
+      view = dequeuedView
+    } else {
+      view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+      view.canShowCallout = true
+      view.calloutOffset = CGPoint(x: -5, y: 5)
+      view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+    }
+    return view
+  }
+    
+    private func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+
+        if control == view.rightCalloutAccessoryView {
+            self.performSegue(withIdentifier: "GoDetail", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "GoDetail" {
+        _ = segue.destination as! DetailViewController
+//        destinationVC.catName = catName
+    }
+//        else if segue.identifier == "goToAdd" {
+//        let destinationVC = segue.destination as! AddViewController
+//    }
+    
+    }
+    
+}
+
+
---
+++ StreetCat/infoViewController.swift
@@ -0,0 +1,12 @@
+//
+//  infoViewController.swift
+//  mapview
+//
+//  Created by ihyemin on 17/11/2019.
+//  Copyright © 2019 ihyemin. All rights reserved.
+//
+
+import UIKit
+
+class infoViewController: UIViewController {}
+
