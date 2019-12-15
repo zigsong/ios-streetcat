5a780884d15068f05c511bcc6e0ce9a1f78bf3d7
ihyemin <lhm0710@naver.com>
2019-12-15 14:30:55 +0900

    before image

--- StreetCats/ViewController.swift
+++ StreetCats/ViewController.swift
@@ -19,10 +19,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ViewToViewDel
     var cats: [CatAnnotation] = []
     // 터치한 핀의 좌표값과 비교, 참조하기 위한 좌표들
     var catSpot: [CLLocationCoordinate2D] = []
-    
-    var catNames: [String] = []
-    var catDetails: [String] = []
-    var catIsLiked: [Bool] = []
     var indexOfCat = 0
     
     var location = "marker"
@@ -113,9 +109,6 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ViewToViewDel
                 print("\(cat.spot.coordinate)")
                 cats += [CatAnnotation(title: cat.name, color: cat.color, spot: CLLocationCoordinate2D(latitude: cat.spot.coordinate.latitude, longitude: cat.spot.coordinate.longitude), coordinate: CLLocationCoordinate2D(latitude: cat.spot.coordinate.latitude, longitude: cat.spot.coordinate.longitude), details: cat.details, isLiked: cat.isLiked)]
                 catSpot += [cat.spot.coordinate]
-                catNames += [cat.name]
-                catDetails += [cat.details]
-                catIsLiked += [cat.isLiked]
             }
                 myMap.addAnnotations(cats)
           } catch {
@@ -126,22 +119,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ViewToViewDel
     // remove하면 새로 추가한 핀은 다시 안 생기네요. savedCats의 파일도 별도로 부르는 게 필요함.
     @IBOutlet weak var likeFilterButton: UIButton!
 
-        @IBAction func likeFilter(_ sender: UIButton) {
-            if likeFilterButton.isSelected == true {
-                myMap.removeAnnotations(cats)
-                myMap.addAnnotations(cats)
-    //            myMap.addAnnotations(<#T##annotations: [MKAnnotation]##[MKAnnotation]#>)
-                likeFilterButton.isSelected = false
-            } else {
-                for CatAnnotation in self.myMap.annotations {
-                      let annotation = CatAnnotation
-                      if (annotation as? CatAnnotation)?.isLiked == false {
-                         self.myMap.removeAnnotation(CatAnnotation)
-                likeFilterButton.isSelected = true
-                    }
+    @IBAction func likeFilter(_ sender: UIButton) {
+        if likeFilterButton.isSelected == true {
+            myMap.removeAnnotations(cats)
+            myMap.addAnnotations(cats)
+            //            myMap.addAnnotations(<#T##annotations: [MKAnnotation]##[MKAnnotation]#>)
+            likeFilterButton.isSelected = false
+        } else {
+            for CatAnnotation in self.myMap.annotations {
+                let annotation = CatAnnotation
+                if (annotation as? CatAnnotation)?.isLiked == false {
+                    self.myMap.removeAnnotation(CatAnnotation)
                 }
             }
+            likeFilterButton.isSelected = true
         }
+    }
     // AddVC의 추가가 완료되면 데이터가 인코딩되고 즉시 바로 myMap으로 디코딩할 때, 작동할 catAdded 함수.
     
     // 맵에서 핀이 찍히지는 않아도 제이슨 파일에는 추가가 되는 듯?
@@ -160,6 +153,9 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ViewToViewDel
                 let newCat = try decoder.decode(Cat.self, from: data)
                 print(newCat) // test
                 let newCatMark = CatAnnotation(title: newCat.name, color: newCat.color, spot: CLLocationCoordinate2D(latitude: newCat.spot.coordinate.latitude, longitude: newCat.spot.coordinate.longitude), coordinate: CLLocationCoordinate2D(latitude: newCat.spot.coordinate.latitude, longitude: newCat.spot.coordinate.longitude), details: newCat.details, isLiked: false)
+                cats += [newCatMark]
+                catSpot += [newCatMark.coordinate]
+                
                 myMap.addAnnotation(newCatMark)
                 print("add cat success")
             }
@@ -169,20 +165,16 @@ class ViewController: UIViewController, CLLocationManagerDelegate, ViewToViewDel
         }
     }
     
-    // DetailVC에서 수정한 isLiked 데이터를 받아오기 위한 함수
-    func isLikedSent(_ data: [Bool]) {
-        catIsLiked = data
-        
-    }
     
+    func isLikedSent(_ data: [CatAnnotation]) {
+        cats = data
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goToInfo", let dest = segue.destination as? DetailViewController {
 
-            dest.catNames = catNames
-            dest.catDetails = catDetails
-            dest.catIsLiked = catIsLiked
             dest.indexOfCat = indexOfCat
+            dest.cats = cats
             dest.delegate = self
         } else if segue.identifier == "goToAdd", let dest = segue.destination as? AddViewController {
             // myMap의 롱프레스 변수를 AddVC에서 쓸 수 있게 AddVC의 롱프레스 변수로 넘겨줌.
@@ -215,7 +207,7 @@ extension ViewController: MKMapViewDelegate {
     else if annotation.color == "gray" { identifier = "Gray"
     color = .gray}
     else {identifier = "else"
-        color = .red }
+        color = .orange }
         
     var view: MKMarkerAnnotationView
     if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
@@ -244,16 +236,11 @@ extension ViewController: MKMapViewDelegate {
         
         // 사용자가 탭한 핀의 좌표를 받음.
         touchedLocation = view.annotation?.coordinate
-        // catSpot에서 탭한 좌표와 일치하는 값을 찾으면 그에 해당하는 ..
         for i in catSpot {
             if i.latitude == touchedLocation!.latitude && i.longitude == touchedLocation!.longitude {
                 indexOfCat = catSpot.firstIndex(where: {$0.latitude == touchedLocation!.latitude && $0.longitude == touchedLocation!.longitude}) ?? 0
             }
         }
-//        print(touchedLocation)
-//        print(catSpot[0])
-//        print(catNames)
-//        print(indexOfCat)
         
         performSegue(withIdentifier: "goToInfo", sender: self)
     }
@@ -270,6 +257,5 @@ extension ViewController: UIGestureRecognizerDelegate {
 // 다른 뷰 간에 데이터를 넘겨 받기 위한 delegate 프로토콜
 protocol ViewToViewDelegate {
     func catAdded()
-    func isLikedSent(_ data: [Bool])
+    func isLikedSent(_ data: [CatAnnotation])
 }
- 
